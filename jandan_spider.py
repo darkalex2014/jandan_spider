58d737cc889758b9c5f8a1689f18d95d987e9612
Iblis <zong2937@163.com>
2019-05-29 18:00:05 +0800

    补充了存放地址，根目录下太乱了

--- jandan_spider.py
+++ jandan_spider.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import os.path
 
 index = 0
 headers = {'referer': 'http://jandan.net/', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'}
@@ -10,15 +12,17 @@ def save_jpg(res_url):
     global index
     html = BeautifulSoup(requests.get(res_url, headers=headers).text)
     for link in html.find_all('a', {'class': 'view_img_link'}):
-        with open('{}.{}'.format(index, link.get('href')[len(link.get('href'))-3: len(link.get('href'))]), 'wb') as jpg:
+        if not os.path.exists('meizi/'):
+            os.makedirs('meizi/')
+        with open('meizi/''{}.{}'.format(index, link.get('href')[len(link.get('href'))-3: len(link.get('href'))]), 'wb') as jpg:
             jpg.write(requests.get("http:" + link.get('href')).content)
         print("正在抓取第%s条数据" % index)
         index += 1
 
 
-#  抓取煎蛋妹子图片，默认抓取5页
+#  抓取煎蛋妹子图片，默认抓取5页,3ye
 if __name__ == '__main__':
     url = 'http://jandan.net/ooxx'
-    for i in range(0, 5):
+    for i in range(0, 3):
         save_jpg(url)
         url = "http:" + BeautifulSoup(requests.get(url, headers=headers).text).find('a', {'class': 'previous-comment-page'}).get('href')
